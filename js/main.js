897d53bb0598c64cc16a9c670823149cf2e8a346
Sandy <estalien.sandy@outlook.com>
2020-09-17 11:56:18 -0700

    modification url mail section on index.html and main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 
+    
+ function redirectionWhatsapp() {
+    window.location.href = "http://wa.me/50934154040";
+ }
+
+ function redirectionMessenger( ){
+    window.location.href = "https://web.facebook.com/gregory.domond";
+ }
+ 
+ function redirectionMail( ){
+    window.location.href = "mailto:info@promotic.net";
+ }
+ 
